e75d9980a1092d70c55f0c76e2a6649767edf5de
Changdong Oh <lamancha91@gmail.com>
2018-06-29 05:45:36 -0700

    - update preprocessing code - add descriptions on data - R codes for stm model and visualization

---
+++ stm_tidyverse.R
@@ -0,0 +1,73 @@
+library(readr)
+library(stringr)
+library(stm)
+library(tm)
+library(Cairo)
+library(extrafont)
+library(xlsx)
+library(tidyverse)
+library(tidytext)
+library(quanteda)
+
+data <- as.data.frame(read_csv("processed_2002.csv"))
+data$press <- as.factor(data$press)
+data$date <- as.factor(data$dindex)
+data = subset(data, select = c(pos, press, date, article))
+
+corpus <- corpus(data$pos)
+docvars(corpus, field='press') <- data$press
+docvars(corpus, field='month') <- as.integer(data$date)
+
+dfm <- dfm(dfm(corpus,
+        tolower=F,
+        stem=F))
+
+stmdfm <- convert(dfm, to = "stm", docvars = docvars(corpus))
+# plotRemoved(stmdfm$documents, lower.thresh = seq(1, 100, by = 10))
+
+out <- prepDocuments(stmdfm$documents, stmdfm$vocab, stmdfm$meta, lower.thresh = 15)
+
+short = strtrim(data$article, 300)
+rm(data, dfm, stmdfm, corpus)
+
+K = 0
+seed = sample(1:10000, 1)
+stmmodel <- stm(out$documents, out$vocab, K = K, prevalence =~ s(month) + press, 
+                data = out$meta, init.type = "Spectral", seed = seed)
+
+save.image(file=paste0(seed, "stmmodel", K, ".rdata"))
+
+topicproportion = colMeans(stmmodel$theta[,seq(1, K, 1)])
+labels = labelTopics(stmmodel, c(1:K), K)
+
+topics = data.frame(matrix(nrow=101), stringsAsFactors = F)[-1]
+for(i in 1:K){
+  topic = as.data.frame(
+    cbind(labels$prob[i, ][1:100], 
+          labels$frex[i, ][1:100],
+          labels$lift[i, ][1:100],
+          labels$score[i, ][1:100]),
+    stringsAsFactors = F)
+  names(topic) = paste(i, c('Prob', 'FREX', 'LIFT', 'Score'))
+  topic = rbind(c(topicproportion[i], NA, NA, NA), topic)
+  topics = cbind(topics, topic)
+}
+
+write.xlsx(topics, paste0(seed, 'Topics ', K, '.xlsx'))
+
+# write topic distribution table
+propbydoc = make.dt(stmmodel)
+write.csv(propbydoc, paste0(seed, 'propbydoc', K, '.csv'), row.names=F)
+
+par("mar")
+par(mar = c(5.1, 4.1, 4.1, 2.1))
+par(oma = c(0, 0, 0, 0))
+
+# size of topics in total documents
+
+cairo_pdf(paste0(seed, "summary", K, ".pdf"), family="Malgun Gothic", width=10, height=15)
+plot(stmmodel, type="summary", text.cex=0.3)
+dev.off()
+
+sample(findThoughts(stmmodel, texts=short, n=300, topics=17
+                    )$docs[[1]], 30)
---
+++ visualization.R
@@ -0,0 +1,255 @@
+library(ggplot2)
+library(dplyr)
+library(readr)
+
+data <- as.data.frame(read_csv("processed_2002.csv"))
+
+# Frequencies of Words in Articles
+
+freq <- read_csv("freq.csv")
+freq = cbind(as.data.frame(seq(1, 100)), freq)
+names(freq) = c('seq', 'word', 'num')
+
+ggplot(data = freq, aes(x=word, y=num)) +
+  geom_bar(stat='identity', colour = '#0066CC', fill = '#0066CC',
+           width = 0.5) +
+  theme(panel.grid.minor = element_blank(), 
+        panel.grid.major = element_line(color = "gray50", size = 0.5),
+        panel.grid.major.x = element_blank(),
+        panel.background = element_blank(),
+        plot.title = element_text(hjust = 0.5),
+        axis.ticks.length = unit(.25, "cm"),
+        axis.text.x = element_text(angle = 90, 
+                                   vjust = 0, hjust = 1, 
+                                   size = 10, face = 'bold'),
+        axis.ticks.y = element_blank(),
+        axis.ticks.x = element_blank()) +
+  scale_x_discrete(limits=freq$word) +
+  labs(list(title = '최빈 단어 100개',
+            x = '단어', 
+            y = '사용된 횟수'))
+
+ggsave('freq.png', width = 30, height=24, dpi = 300, units="cm")
+
+# Number of Articles by Year
+numbers = data.frame(matrix(ncol=2))[-1,]
+for(i in 2002:2018){
+  numbers = rbind(numbers,
+                  cbind(nrow(filter(data, grepl(as.character(i), date))), i))
+}
+crimenumbers = c(967+998+5581, 
+                 7596+857+1065, #14231
+                 8558+749+910,
+                 967+832+9841,
+                 9383+869+1008,
+                 1093+1011+10508,
+                 11380+1138+914,
+                 1124+1295+12895,
+                 13110+1428+1020,
+                 1063+1770+13352,
+                 1437+2651+19566,
+                 20101+2262+1132,
+                 1356+1632+21834,
+                 2227+1573+28883,
+                 3055+1534+26835,
+                 5004+1249+31828,
+                 14398+617+2313)
+numbers = cbind(numbers, crimenumbers)
+names(numbers) = c('num', 'time', 'cnum')
+
+ggplot(data = numbers, 
+       mapping = aes(x = time, y = num, group = 1)) +
+  geom_line(lwd = 1, color = 'blue') +
+  theme(panel.grid.minor = element_blank(), 
+        panel.grid.major = element_line(color = "gray50", size = 0.5),
+        panel.grid.major.x = element_blank(),
+        panel.background = element_blank(),
+        plot.title = element_text(hjust = 0.5),
+        axis.ticks.length = unit(.25, "cm"),
+        axis.text.x = element_text(angle = 90, hjust = 1, size = 11, face='bold'),
+        axis.ticks.y = element_blank(),
+        axis.ticks.x = element_blank()) +
+  labs(list(title = '연도별 성범죄 관련 기사 숫자',
+            x = '시기(연도)', 
+            y = '기사 숫자'))
+ggsave('sexualharassment.png', width = 30, height=24, dpi = 300, units="cm")
+
+ggplot(data = numbers, 
+       mapping = aes(x = time, y = cnum, group = 1)) +
+  geom_line(lwd = 1, color = 'red') +
+  theme(panel.grid.minor = element_blank(), 
+        panel.grid.major = element_line(color = "gray50", size = 0.5),
+        panel.grid.major.x = element_blank(),
+        panel.background = element_blank(),
+        plot.title = element_text(hjust = 0.5),
+        axis.ticks.length = unit(.25, "cm"),
+        axis.text.x = element_text(angle = 90, hjust = 1, size = 11, face='bold'),
+        axis.ticks.y = element_blank(),
+        axis.ticks.x = element_blank()) +
+  labs(list(title = '연도별 일반 범죄 관련 기사 숫자',
+            x = '시기(연도)', 
+            y = '기사 숫자'))
+ggsave('crime.png', width = 30, height=24, dpi = 300, units="cm")
+
+
+topics <- read_csv("topicnadmeta.csv") 
+topics$month = as.factor(floor(topics$month/3))
+topics$press = topics$press %>% as.factor()
+
+fil_data = as.data.frame(matrix(ncol=98))[-1,]
+for(i in levels(topics$month)){
+  for(j in levels(topics$press)){
+    sub_set = filter(topics, month == i & press == j)
+    sub_set[1:95] = sub_set[1:95]*sub_set$num
+    metalevel_subset = data.frame(t(colSums(sub_set[1:95])))
+    metalevel_subset$month = i
+    metalevel_subset$press = j
+    metalevel_subset$num = sum(sub_set$num)
+    fil_data = rbind(fil_data, metalevel_subset)
+  }
+}
+
+df = fil_data
+df = filter(df, num != 0)
+df$month = as.integer(df$month)
+
+solution_punish = df$Topic1 + df$Topic5 + df$Topic29 + df$Topic65 + df$Topic69 + df$Topic76
+solution_str = df$Topic2 + df$Topic35 + df$Topic49 + df$Topic55 + df$Topic70 + df$Topic88
+sens_depiction = df$Topic28 + df$Topic36 + df$Topic61 + df$Topic75
+empathy_nar = df$Topic51 + df$Topic73 + df$Topic80
+objective_desc = df$Topic10 + df$Topic23 + df$Topic27 + df$Topic43 + 
+  df$Topic45 + df$Topic60 + df$Topic64 + df$Topic67 + df$Topic87
+mental_disorder = df$Topic18 + df$Topic20
+acquaintance_repeat = df$Topic30 + df$Topic84
+intimate_partner = df$Topic14 + df$Topic41 
+space_world = df$Topic4 + df$Topic6 + df$Topic9 + df$Topic25 + df$Topic40 + df$Topic58 + 
+  df$Topic62 + df$Topic74 + df$Topic82 + df$Topic91 + df$Topic92 + df$Topic93
+space_organization = df$Topic8 + df$Topic38 + df$Topic71
+space_politics = df$Topic12 + df$Topic37 + df$Topic46 + df$Topic54 + df$Topic77 + df$Topic94
+space_art = df$Topic13 + df$Topic17 + df$Topic53 + df$Topic68
+space_sports_entertainment = df$Topic3 + df$Topic34 + df$Topic79
+space_polmil = df$Topic16 + df$Topic47
+space_work_emotion = df$Topic19 + df$Topic39 + df$Topic50 
+space_digital = df$Topic21
+space_school = df$Topic42 + df$Topic57 + df$Topic63
+space_public = df$Topic48 + df$Topic78
+space_professional = df$Topic59 + df$Topic86 + df$Topic31 + df$Topic24 + 
+  df$Topic26 + df$Topic32
+
+df = cbind(solution_punish, solution_str, sens_depiction, empathy_nar, objective_desc,
+           mental_disorder, acquaintance_repeat, intimate_partner, space_art, space_digital,
+           space_organization, space_politics, space_polmil, space_professional, 
+           space_public, space_school, space_sports_entertainment, space_work_emotion,
+           space_world, df[96:98])
+
+sub_data = df %>% filter(grepl('', press))
+sub_data = sub_data[1:20] %>%
+  group_by(month) %>% 
+  summarise_all(funs(sum))
+sub_data[2:20] = sub_data[2:20]/rowSums(sub_data[2:20])
+
+library(reshape2)
+
+d = sub_data %>% select(c('solution_punish', 'solution_str',
+                          'month'))
+names(d) = c('대책: 가해자 처벌', '대책: 피해자 보호 및 구조', 'month')
+d = melt(d, id.vars='month')
+ggplot(data = d, 
+       mapping = aes(x = month, y = value, fill = variable, color = variable)) +
+  geom_line(lwd = 1) +
+  scale_colour_discrete(name = '프레임') +
+  theme(panel.grid.minor = element_blank(), 
+        panel.grid.major = element_line(color = "gray50", size = 0.5),
+        panel.grid.major.x = element_blank(),
+        panel.background = element_blank(),
+        plot.title = element_text(hjust = 0.5),
+        axis.ticks.length = unit(.25, "cm"),
+        axis.text.x = element_text(angle = 90, hjust = 1, size = 11, face='bold'),
+        axis.ticks.y = element_blank(),
+        axis.ticks.x = element_blank()) + 
+  coord_cartesian(xlim = c(-0.0001, 67), 
+                  ylim = c(-max(d$value)*0.08, max(d$value)*1.11), expand = FALSE) +
+  labs(list(title = paste0('시간 변화에 따른 토픽 규모 변화'),
+            x = '시기(연도 및 월)', 
+            y = '토픽이 차지하는 비율')) +
+  scale_x_continuous(breaks = (c(0,50)), labels=c('', '')) +
+  annotate(geom = 'text', 
+           x = seq(1, 66, 1),
+           y = -max(d$value)*0.03, size = 2,
+           label = c(rep(c('1', '4', '7', '10'), 16), '1', '4')) +
+  annotate(geom = 'text', 
+           x = seq(2, 66, 4),
+           y = -max(d$value)*0.06, size = 3,
+           label = seq(2002, 2018, 1))
+ggsave('solutions.png', width = 30, height=24, dpi = 300, units="cm")
+sub_data
+
+d = sub_data %>% select(c('sens_depiction', 'empathy_nar', 'objective_desc',
+                          'month'))
+names(d) = c('묘사: 자극적 표현 중', '묘사: 내러티브와 동정심', 
+             '묘사: 사실관계 위주 서술', 'month')
+d = melt(d, id.vars='month')
+ggplot(data = d, 
+       mapping = aes(x = month, y = value, fill = variable, color = variable)) +
+  geom_line(lwd = 1) +
+  scale_colour_discrete(name = '프레임') +
+  theme(panel.grid.minor = element_blank(), 
+        panel.grid.major = element_line(color = "gray50", size = 0.5),
+        panel.grid.major.x = element_blank(),
+        panel.background = element_blank(),
+        plot.title = element_text(hjust = 0.5),
+        axis.ticks.length = unit(.25, "cm"),
+        axis.text.x = element_text(angle = 90, hjust = 1, size = 11, face='bold'),
+        axis.ticks.y = element_blank(),
+        axis.ticks.x = element_blank()) + 
+  coord_cartesian(xlim = c(-0.0001, 67), 
+                  ylim = c(-max(d$value)*0.08, max(d$value)*1.11), expand = FALSE) +
+  labs(list(title = paste0('시간 변화에 따른 토픽 규모 변화'),
+            x = '시기(연도 및 월)', 
+            y = '토픽이 차지하는 비율')) +
+  scale_x_continuous(breaks = (c(0,50)), labels=c('', '')) +
+  annotate(geom = 'text', 
+           x = seq(1, 66, 1),
+           y = -max(d$value)*0.03, size = 2,
+           label = c(rep(c('1', '4', '7', '10'), 16), '1', '4')) +
+  annotate(geom = 'text', 
+           x = seq(2, 66, 4),
+           y = -max(d$value)*0.06, size = 3,
+           label = seq(2002, 2018, 1))
+ggsave('images.png', width = 30, height=24, dpi = 300, units="cm")
+
+d = sub_data %>% select(c('mental_disorder', 'acquaintance_repeat', 'intimate_partner',
+                          'month'))
+names(d) = c('정신병력과 음주', '계획적, 상습적 범죄', 
+             '연인간 성범죄', 'month')
+d = melt(d, id.vars='month')
+ggplot(data = d, 
+       mapping = aes(x = month, y = value, fill = variable, color = variable)) +
+  geom_line(lwd = 1) +
+  scale_colour_discrete(name = '프레임') +
+  theme(panel.grid.minor = element_blank(), 
+        panel.grid.major = element_line(color = "gray50", size = 0.5),
+        panel.grid.major.x = element_blank(),
+        panel.background = element_blank(),
+        plot.title = element_text(hjust = 0.5),
+        axis.ticks.length = unit(.25, "cm"),
+        axis.text.x = element_text(angle = 90, hjust = 1, size = 11, face='bold'),
+        axis.ticks.y = element_blank(),
+        axis.ticks.x = element_blank()) + 
+  coord_cartesian(xlim = c(-0.0001, 67), 
+                  ylim = c(-max(d$value)*0.08, max(d$value)*1.11), expand = FALSE) +
+  labs(list(title = paste0('시간 변화에 따른 토픽 규모 변화'),
+            x = '시기(연도 및 월)', 
+            y = '토픽이 차지하는 비율')) +
+  scale_x_continuous(breaks = (c(0,50)), labels=c('', '')) +
+  annotate(geom = 'text', 
+           x = seq(1, 66, 1),
+           y = -max(d$value)*0.03, size = 2,
+           label = c(rep(c('1', '4', '7', '10'), 16), '1', '4')) +
+  annotate(geom = 'text', 
+           x = seq(2, 66, 4),
+           y = -max(d$value)*0.06, size = 3,
+           label = seq(2002, 2018, 1))
+ggsave('stereotypes_criminal.png', width = 30, height=24, dpi = 300, units="cm")
+
+
